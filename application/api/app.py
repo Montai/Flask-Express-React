80d89622d0436d8e289bd758e6c0064e57601869
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-06-17 15:56:16 -0700

    add Flask-CORS to requirements.txt, styled-components and superagent to package.json. The example frontend now uses the example API to generate a Hello message

--- application/api/app.py
+++ application/api/app.py
@@ -2,8 +2,10 @@
 __all__ = ['app']
 
 import flask
+import flask_cors
 from urllib.parse import urlparse
 from .config import config
 
 app = flask.Flask(__name__)
 app.config['SERVER_NAME'] = urlparse(config['api']['serverName']).netloc
+flask_cors.CORS(app)
--- application/web/index.js
+++ application/web/index.js
@@ -1,11 +1,79 @@
 
+import config from '../config.json'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+import superagent from 'superagent'
+
+const API = config.web.internalApiServerName || config.api.serverName
+
+const IndexStyle = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  background-color: #2b2b2b;
+
+  > div {
+    padding: 1.5em;
+    background-color: #eee;
+  }
+
+  .title {
+    font-size: 120%;
+    font-weight: bold;
+    margin-top: 0;
+  }
+
+  input[type=text] {
+    padding: .5em;
+    border-radius: 2px;
+    border: 1px solid #aaa;
+    margin: 0.5em;
+  }
+
+  button {
+    padding: 0.5em;
+    margin: 0.5em;
+    float: right;
+    border-radius: 2px;
+    border: 1px solid #0333b2;
+    background-color: #117ee3;
+    color: white;
+    cursor: pointer;
+
+    :hover {
+      transition: 0.2s;
+      background-color: #2188d2;
+    }
+  }
+`
 
 export class IndexComponent extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.name = React.createRef()
+  }
+
+  async send(ev) {
+    ev.preventDefault()
+    let name = this.name.current.value;
+    let result = await superagent.get(API + '/api/hello', {'name': name})
+    alert(result.body.message)
+  }
+
   render() {
-    return <p>Hello, World</p>
+    return <IndexStyle>
+      <div>
+        <p className="title">Hello API Test</p>
+        <form>
+          <label htmlFor="name">Name</label>
+          <input ref={this.name} type="text" name="name" autoFocus/> <br/>
+          <button onClick={this.send.bind(this)}>Send</button>
+        </form>
+      </div>
+    </IndexStyle>
   }
 
 }
